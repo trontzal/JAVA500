6e7d4c74a813f4655ae5808a8253fb4bc93eb8bc
java <java@DESKTOP-BK63CH3.ipartekaula.com>
2024-01-17 14:19:24 +0100

    Insert(comentado)

--- ejemploBasicoJdbc/UF2213_mio/src/com/ipartek/formacion/uf2213/EjemploJDBC.java
+++ ejemploBasicoJdbc/UF2213_mio/src/com/ipartek/formacion/uf2213/EjemploJDBC.java
@@ -13,20 +13,25 @@ public class EjemploJDBC {
 		final String PASS = "1234";
 
 		String SQL_SELECT = "SELECT * FROM clientes";
+//		String SQL_INSERT = "INSERT INTO clientes(dni, nombre, apellidos, fecha_nacimiento) VALUES ('12548796w', 'Gon', 'Lecumberri', '1996-08-19')";
 
 		final Connection con = DriverManager.getConnection(URL, USER, PASS);
 		final Statement st = con.createStatement();
+
+//		int filasAfectadas = st.executeUpdate(SQL_INSERT);
+
+//		if (filasAfectadas > 0) {
+//			System.out.println("Insert exitoso");
+//		} else {
+//			System.out.println("Error al isnertar");
+//		}
+
 		ResultSet rs = st.executeQuery(SQL_SELECT);
 
 		while (rs.next()) {
-			System.out.printf("%2s %s %4s %-8s %-10s %s\n",
-				rs.getLong("id"),
-				rs.getString("dni"),
-				rs.getString("dni_diferencial"),
-				rs.getString("nombre"),
-				rs.getString("apellidos"),
-				rs.getString("fecha_nacimiento")
-			);
+			System.out.printf("%2s %s %4s %-8s %-10s %s\n", rs.getLong("id"), rs.getString("dni"),
+					rs.getString("dni_diferencial"), rs.getString("nombre"), rs.getString("apellidos"),
+					rs.getString("fecha_nacimiento"));
 		}
 	}
 }
